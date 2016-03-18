625b152e8f2f346d65ac40d34db5f897c88e79b8
Loic Donkeng <Loic Donkeng@141.19.165.131>
2016-03-18 18:47:26 +0100

    need of implementation of some methods in the other classes.

--- src/tpe/del/grp7/ub1/a3/Betrag.java
+++ src/tpe/del/grp7/ub1/a3/Betrag.java
@@ -2,6 +2,15 @@ package tpe.del.grp7.ub1.a3;
 
 class Betrag {
 
-	public Betrag() {
+	public Betrag(long betrag, Waehrung waehrung) {
+		
+	}
+
+	public Waehrung getWaehrung() {
+		return null;
+	}
+
+	public long getBetrag() {
+		return 0;
 	}
 }
--- src/tpe/del/grp7/ub1/a3/Waehrung.java
+++ src/tpe/del/grp7/ub1/a3/Waehrung.java
@@ -4,4 +4,19 @@ class Waehrung {
 
 	public Waehrung() {
 	}
+
+	public long umrechnen(long betrag, Waehrung waehrung) {
+		return betrag;
+		
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getKuerzel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

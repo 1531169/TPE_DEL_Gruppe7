5902759d207cbaa5639b82910f05dce842a09128
Tobias Brückner <1525082@stud.hs-mannheim.de>
2016-03-20 10:35:22 +0100

    Worked on class Waehrung:
    - added attributes (name, kuerzel, kurs) and their getters
    - added auto-generated methods equals(Object o) and hashCode()

--- src/tpe/del/grp7/ub1/a3/Waehrung.java
+++ src/tpe/del/grp7/ub1/a3/Waehrung.java
@@ -1,7 +1,18 @@
 package tpe.del.grp7.ub1.a3;
 
+/**
+ * In dieser Klasse liegen die Daten zu einer Währung.
+ * 
+ * @author Gruppe 7
+ * @version 20.03.2016
+ */
+
 class Waehrung {
 
+	private String name;
+	private String kuerzel;
+	private double kurs;
+	
 	public Waehrung() {
 	}
 
@@ -10,13 +21,78 @@ class Waehrung {
 		
 	}
 
+	/**
+	 * Getter-Methode für name
+	 * 
+	 * @return gibt Instanzvariable name zurück
+	 */
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
+	/**
+	 * Getter-Methode für kuerzel
+	 * 
+	 * @return gibt Instanzvariable kuerzel zurück
+	 */
 	public String getKuerzel() {
-		// TODO Auto-generated method stub
-		return null;
+		return kuerzel;
+	}
+
+	/**
+	 * Getter-Methode für kurs
+	 * 
+	 * @return gibt Instanzvariable kurs zurück
+	 */
+	public double getKurs() {
+		return kurs;
+	}
+
+	
+	/**
+	 * Methode, die einen Hash-Wert über das Objekt berechnet.
+	 * 
+	 * @Override
+	 * @return gibt Hash-Wert zurück 
+	 */
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kuerzel == null) ? 0 : kuerzel.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/**
+	 * Methode, die den Inhalt eines Objekts mit einem anderen vergleicht.
+	 * 
+	 * @Override
+	 * @param Objekt, mit dem verglichen wird
+	 * @return gibt Auskunft über die Gleichheit der beiden Objekten in Form von true oder false
+	 */
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Waehrung other = (Waehrung) obj;
+		if (kuerzel == null) {
+			if (other.kuerzel != null)
+				return false;
+		} else if (!kuerzel.equals(other.kuerzel))
+			return false;
+		if (Double.doubleToLongBits(kurs) != Double.doubleToLongBits(other.kurs))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 }

ec352f5ab7944bb16d3c7c4b6d9bc3422cd4925d
Cedric <Cedric@Cedric-PC.fritz.box>
2016-03-28 23:57:41 +0200

    finished test for betrag class

--- src/tpe/del/grp7/ub1/a3/BetragTest.java
+++ src/tpe/del/grp7/ub1/a3/BetragTest.java
@@ -1,13 +1,12 @@
 package tpe.del.grp7.ub1.a3;
 
 import org.junit.*;
-import org.junit.rules.ExpectedException;
 
 public class BetragTest {
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void testConstructorThrowsInvalidArgumentException() {
-		new Betrag(12345678L, null);
+		new Betrag(12345678, null);
 	}
 	
 	@Test 
@@ -15,7 +14,7 @@ public class BetragTest {
 		boolean thrown = false;
 		
 		try {
-			new Betrag(1234L, new Waehrung("Test", "T", 0.1234));
+			new Betrag(1234, new Waehrung("Test", "T", 0.1234));
 		} catch (IllegalArgumentException e) {
 			thrown = true;
 		}
@@ -33,15 +32,17 @@ public class BetragTest {
 	@Test
 	public void testConstructorRightWaehrungen() {
 		Waehrung expected = Waehrungen.dollar;
-		Betrag b = new Betrag(2345L, Waehrungen.dollar);
+		Betrag b = new Betrag(2345, Waehrungen.dollar);
 		Waehrung actual = b.getWaehrung();
 		Assert.assertEquals(expected, actual);
 	}
 	
+	
+	
 	@Test
 	public void testGetVorzeichenWithPosLong() {
 		int expected = 1;
-		Betrag b = new Betrag(123456L, Waehrungen.euro);
+		Betrag b = new Betrag(123456, Waehrungen.euro);
 		int actual = b.getVorzeichen();
 		Assert.assertEquals(expected, actual);
 	}
@@ -49,7 +50,7 @@ public class BetragTest {
 	@Test
 	public void testGetVorzeichenWithNegLong() {
 		int expected = -1;
-		Betrag b = new Betrag(-123456L, Waehrungen.euro);
+		Betrag b = new Betrag(-123456, Waehrungen.euro);
 		int actual = b.getVorzeichen();
 		Assert.assertEquals(expected, actual);
 	}
@@ -81,8 +82,278 @@ public class BetragTest {
 	@Test
 	public void testGetVorzeichenWithZeroLong() {
 		int expected = 1;
-		Betrag b = new Betrag(0000L, Waehrungen.euro);
+		Betrag b = new Betrag(0000, Waehrungen.euro);
 		int actual = b.getVorzeichen();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	
+	@Test
+	public void testAddiereNull() {
+		long expected = 1000;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag result = b.addiere(null);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testAddiereMitGleichenWaehrungen() {
+		long expected = 1120;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag c = new Betrag(120, Waehrungen.euro);
+		Betrag result = b.addiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testAddiereMitVerschWaehrungen() {
+		long expected = 1094;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag c = new Betrag(120, Waehrungen.dollar);
+		Betrag result = b.addiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testAddiereMitVerschWaehrungen2() {
+		long expected = 1002;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag c = new Betrag(120, Waehrungen.rubel);
+		Betrag result = b.addiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testAddiereMitGleichenWaehrungenNeg() {
+		long expected = 1000;
+		Betrag b = new Betrag(1200, Waehrungen.euro);
+		Betrag c = new Betrag(-200, Waehrungen.euro);
+		Betrag result = b.addiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testAddiereMitVerschWaehrungenNeg() {
+		long expected = 998;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag c = new Betrag(-120, Waehrungen.rubel);
+		Betrag result = b.addiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	
+	@Test
+	public void testSubtrahiereNull() {
+		long expected = 1000;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag result = b.subtrahiere(null);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testSubtrahiereMitGleichenWaehrungen() {
+		long expected = 1000;
+		Betrag b = new Betrag(1120, Waehrungen.euro);
+		Betrag c = new Betrag(120, Waehrungen.euro);
+		Betrag result = b.subtrahiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testSubtrahiereMitVerschWaehrungen() {
+		long expected = 1094;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag c = new Betrag(120, Waehrungen.dollar);
+		Betrag result = b.addiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testSubtrahiereMitVerschWaehrungen2() {
+		long expected = 1000;
+		Betrag b = new Betrag(1002, Waehrungen.euro);
+		Betrag c = new Betrag(120, Waehrungen.rubel);
+		Betrag result = b.subtrahiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testSubtrahiereMitGleichenWaehrungenNeg() {
+		long expected = 1200;
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		Betrag c = new Betrag(-200, Waehrungen.euro);
+		Betrag result = b.subtrahiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testSubtrahiereMitVerschWaehrungenNeg() {
+		long expected = 1000;
+		Betrag b = new Betrag(998, Waehrungen.euro);
+		Betrag c = new Betrag(-120, Waehrungen.rubel);
+		Betrag result = b.subtrahiere(c);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testMultiplizierePos() {
+		long expected = 2500;
+		Betrag b = new Betrag(1000, Waehrungen.franken);
+		Betrag result = b.multipliziere(2.5);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testMultipliziereNeg() {
+		long expected = 5000;
+		Betrag b = new Betrag(1000, Waehrungen.franken);
+		Betrag result = b.multipliziere(5);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testMultipliziereNull() {
+		long expected = 0;
+		Betrag b = new Betrag(1000, Waehrungen.franken);
+		Betrag result = b.multipliziere(0);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testProzentPos() {
+		long expected = 105;
+		Betrag b = new Betrag(1000, Waehrungen.dollar);
+		Betrag result = b.prozent(10.5);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testProzentNeg() {
+		long expected = -105;
+		Betrag b = new Betrag(1000, Waehrungen.dollar);
+		Betrag result = b.prozent(-10.5);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testProzentNull() {
+		long expected = 0;
+		Betrag b = new Betrag(1000, Waehrungen.dollar);
+		Betrag result = b.prozent(0);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testPromillePos() {
+		long expected = 10;
+		Betrag b = new Betrag(1000, Waehrungen.dollar);
+		Betrag result = b.promille(10.5);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testPromilleNeg() {
+		long expected = -10;
+		Betrag b = new Betrag(1000, Waehrungen.dollar);
+		Betrag result = b.promille(-10.5);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testPromilleNull() {
+		long expected = 0;
+		Betrag b = new Betrag(1000, Waehrungen.dollar);
+		Betrag result = b.promille(0);
+		Assert.assertEquals(expected, result.getBetrag());
+	}
+	
+	@Test
+	public void testGetVorkommaPos() {
+		long expected = 14356;
+		Betrag b = new Betrag(14356.45, Waehrungen.rubel);
+		Assert.assertEquals(expected, b.getVorkomma());
+	}
+	
+	@Test
+	public void testGetVorkommaNeg() {
+		long expected = 14356;
+		Betrag b = new Betrag(-14356.50, Waehrungen.rubel);
+		Assert.assertEquals(expected, b.getVorkomma());
+	}
+	
+	@Test
+	public void testGetVorkommaNull() {
+		long expected = 0;
+		Betrag b = new Betrag(0.9, Waehrungen.rubel);
+		Assert.assertEquals(expected, b.getVorkomma());
+	}
+	
+	@Test
+	public void testGetNachkommaPos() {
+		int expected = 45;
+		Betrag b = new Betrag(14356.45, Waehrungen.rubel);
+		Assert.assertEquals(expected, b.getNachkomma());
+	}
+	
+	@Test
+	public void testGetNachkommaNeg() {
+		int expected = 50;
+		Betrag b = new Betrag(-14356.50, Waehrungen.rubel);
+		Assert.assertEquals(expected, b.getNachkomma());
+	}
+	
+	@Test
+	public void testGetNachkommaNull() {
+		long expected = 90;
+		Betrag b = new Betrag(0.9, Waehrungen.rubel);
+		Assert.assertEquals(expected, b.getNachkomma());
+	}
+	
+	@Test
+	public void testGetAsDoublePos() {
+		double expected = 0.9;
+		Betrag b = new Betrag(90, Waehrungen.rubel);
+		double actual = b.getAsDouble();
+		Assert.assertEquals(expected, actual, 0.001);
+	}
+	
+	@Test
+	public void testGetAsDoubleNeg() {
+		double expected = -0.9;
+		Betrag b = new Betrag(-90, Waehrungen.rubel);
+		double actual = b.getAsDouble();
+		Assert.assertEquals(expected, actual, 0.001);
+	}
+	
+	@Test
+	public void testGetAsDoubleNull() {
+		double expected = 0;
+		Betrag b = new Betrag(0, Waehrungen.rubel);
+		double actual = b.getAsDouble();
+		Assert.assertEquals(expected, actual, 0.001);
+	}
+	
+	@Test
+	public void testToStringPos() {
+		String expected = "10.00 €";
+		Betrag b = new Betrag(1000, Waehrungen.euro);
+		String actual = b.toString();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testToStringNeg() {
+		String expected = "-10.00 €";
+		Betrag b = new Betrag(-1000, Waehrungen.euro);
+		String actual = b.toString();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testToStringNull() {
+		String expected = "0.00 CHF";
+		Betrag b = new Betrag(0, Waehrungen.franken);
+		String actual = b.toString();
 		Assert.assertEquals(expected, actual);
 	}
 }

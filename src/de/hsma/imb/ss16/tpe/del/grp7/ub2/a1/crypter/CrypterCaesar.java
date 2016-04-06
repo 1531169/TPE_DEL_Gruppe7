a71b1115183b1b0a253f1214bec76fc2d1e29008
Loic Donkeng <Loic Donkeng@Lenovo-PC>
2016-04-06 17:33:24 +0200

    correction of the classname (from Saesar to Caesar).

--- src/de/hsma/imb/ss16/tpe/del/grp7/ub2/a1/crypter/CrypterCaesar.java
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub2/a1/crypter/CrypterCaesar.java
@@ -1,12 +1,14 @@
 package de.hsma.imb.ss16.tpe.del.grp7.ub2.a1.crypter;
 
-class CrypterSaesar implements Crypter{
+class CrypterCaesar implements Crypter{
 
 	private static final char [] klartext =  {'A','B','C','D','E','F','G','H'
 											 ,'I','J','K','L','M','N','O','P','Q'
 											 ,'R','S','T','U','V','W','X','Y','Z'
 											 };
-	CrypterSaesar(char klarBuchstabe, Key myKey){
+	
+	
+	CrypterCaesar(char klarBuchstabe, Key myKey){
 		
 	}
 	@Override

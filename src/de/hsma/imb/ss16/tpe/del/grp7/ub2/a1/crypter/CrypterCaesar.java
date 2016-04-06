1fa0de806df0f433bc00419e4d059cef8e120036
Loic Donkeng <Loic Donkeng@Lenovo-PC>
2016-04-06 18:38:12 +0200

    Implementierung der Klasse CrypterCaesar.

--- src/de/hsma/imb/ss16/tpe/del/grp7/ub2/a1/crypter/CrypterCaesar.java
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub2/a1/crypter/CrypterCaesar.java
@@ -1,31 +1,49 @@
 package de.hsma.imb.ss16.tpe.del.grp7.ub2.a1.crypter;
 
-class CrypterCaesar implements Crypter{
+class CrypterCaesar implements Crypter {
+	private final int defaultIndex = -1;
+	private final char[] geheimChar = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+			'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+	
+	private int key;
 
-	private static final char [] klartext =  {'A','B','C','D','E','F','G','H'
-											 ,'I','J','K','L','M','N','O','P','Q'
-											 ,'R','S','T','U','V','W','X','Y','Z'
-											 };
-	
-	
-	CrypterCaesar(char klarBuchstabe, Key myKey){
-		
+	CrypterCaesar(Key myKey) {
+		//	 TODO: Validierung
+		this.key = getIndex(myKey.getKey().charAt(0));
 	}
+	
+	private int getKey() {
+		return key;
+	}
+
+	int getIndex(char key) {
+		for (int i = 0; i < geheimChar.length; i++) {
+			if (geheimChar[i] == key) {
+				return i + 1;
+			}
+		}
+		return defaultIndex;
+	}
+
 	@Override
 	public void reset() {
-		
+
 	}
 
 	@Override
 	public char verschluesseln(char klartextZeichen) throws CrypterException {
+		int newIndex = (getIndex(klartextZeichen)+ getKey()) % geheimChar.length;
+		return geheimChar[newIndex];
 		
-		return 0;
 	}
 
 	@Override
 	public char entschluesseln(char cypherZeichen) throws CrypterException {
-		
-		return 0;
+		int newIndex = (getIndex(cypherZeichen)- getKey());
+		if(newIndex < 0){
+			newIndex = newIndex + geheimChar.length;
+		}
+		return geheimChar[newIndex];
 	}
-	
+
 }

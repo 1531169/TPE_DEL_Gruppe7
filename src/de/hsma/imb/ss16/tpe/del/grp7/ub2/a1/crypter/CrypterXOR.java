0c9a824e031d0c5e9200b8724e325479149b0134
Loic Donkeng <Loic Donkeng@Lenovo-PC>
2016-04-13 17:39:35 +0200

    modification of the CrypterXOR

--- src/de/hsma/imb/ss16/tpe/del/grp7/ub2/a1/crypter/CrypterXOR.java
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub2/a1/crypter/CrypterXOR.java
@@ -1,39 +1,18 @@
 package de.hsma.imb.ss16.tpe.del.grp7.ub2.a1.crypter;
 
+import java.util.Stack;
+
 
 public class CrypterXOR implements Crypter {
 
 	private String myKey;
-	private String geheimText;
+	private Stack<Character> hilftext = new Stack<Character>();
 	private static final char[] klarChar = { '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 			'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_' };
-
-	public CrypterXOR(Key myKey) throws CrypterException {
-		
+	
+	
+	public CrypterXOR(Key myKey){
 		this.myKey = myKey.getKey();
-		this.geheimText = createGeheimText(); 
-	}
-	
-	private String createKeyText(String text){
-		String keyText = "";
-		int i = 0;
-		while(i < klarChar.length) {
-			for (int j = 0; j < text.length() && i < klarChar.length; j++) {
-				keyText = keyText + text.charAt(j);
-				i++;
-			}
-		}
-		System.out.println(keyText);
-		return keyText;
-	}
-	
-	private String createGeheimText() throws CrypterException{
-		String geheim = "";
-		for(int i = 1; i < klarChar.length-6; i++ ){
-			geheim = geheim + verschluesseln(klarChar[i]);
-		}
-		System.out.println(geheim);
-		return geheim;
 		
 	}
 
@@ -52,45 +31,40 @@ public class CrypterXOR implements Crypter {
 
 	}
 
+	
 	@Override
 	public char verschluesseln(char klartextZeichen) throws CrypterException {
 		
 		int index = getIndex(klartextZeichen);
-		int index2 = getIndex(myKey.charAt(0)) ;
+		int verschiebung = index-1;
+		char letterOfKey = myKey.charAt(verschiebung%myKey.length()) ;
+		hilftext.push(letterOfKey);
+		int index2 = getIndex(letterOfKey);
 		return klarChar[index^index2];
 	}
 
 	public static void main (String []args) throws CrypterException{
-	CrypterXOR test = new CrypterXOR(new Key("QWERTZUIOPASDFGHJKLYXCVBNM"));
-	String text = "PUFVQ\\RAFZJ_IHHX[Y_MMUAZW";
+	CrypterXOR test = new CrypterXOR(new Key("TOBIASDDGFDJF"));
+	String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	String text2 = "";
+	String text3 = "";
 	for(int i = 0; i < text.length(); i++){
-		System.out.print(test.entschluesseln(text.charAt(i)));
-		System.out.println();
+		text2 += test.verschluesseln(text.charAt(i));
 	}
-	char t;
-	System.out.println(t = test.verschluesseln('D'));
-	System.out.println(test.entschluesseln(t));
+	System.out.println(text2);
+	
+	for(int i = text2.length()-1; i >= 0 ; i--){
+		text3 = test.entschluesseln(text2.charAt(i))+text3;
+	}
+	System.out.println(text3);
 }
 	@Override
 	public char entschluesseln(char cypherZeichen) throws CrypterException {
-		
-		char zeichen = '0';
-		for(int i = 1; i < 27;i++  ){
-			if(verschluesseln(klarChar[i]) == cypherZeichen){
-				return klarChar[i];
-			}
-		}
-		return zeichen;
-		
-//		while(zeichen != cypherZeichen){
-//			zeichen = geheimText.charAt(++i); 
-//		}
-//		
-//		int index = getIndex(cypherZeichen);
-//		System.out.println("index = " + index);
-//		int index2 = getIndex(myKey.charAt(i));
-//		System.out.println("index2 = " + index2);
-//		return klarChar[index^index2];
+		char letterOfKey = hilftext.pop();		
+//		System.out.print("["+letterOfKey);
+//		System.out.println(cypherZeichen + "]");
+		char holdLetter = klarChar[getIndex(cypherZeichen)^getIndex(letterOfKey)];
+		return holdLetter;
 	}
 
 }

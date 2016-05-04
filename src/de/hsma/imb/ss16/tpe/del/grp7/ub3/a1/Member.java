87ba58998969b8c8ba2b9fac7f01228bbe5acf9d
Loic Donkeng <Loic Donkeng@141.19.176.157>
2016-05-04 14:48:44 +0200

    creation of the classes.

---
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub3/a1/Member.java
@@ -0,0 +1,7 @@
+package de.hsma.imb.ss16.tpe.del.grp7.ub3.a1;
+
+public class Member {
+	private int memberID;
+	private String surname;
+
+}
---
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub3/a1/MembershipList.java
@@ -0,0 +1,5 @@
+package de.hsma.imb.ss16.tpe.del.grp7.ub3.a1;
+
+public class MembershipList {
+
+}

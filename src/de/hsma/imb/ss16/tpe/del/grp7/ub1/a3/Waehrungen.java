7813754d4633e1e7c981f969966d867aa9647c11
Loic Donkeng <Loic Donkeng@141.19.176.157>
2016-05-04 12:42:54 +0200

    changed encoding to UTF-8

--- src/de/hsma/imb/ss16/tpe/del/grp7/ub1/a3/Waehrungen.java
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub1/a3/Waehrungen.java
@@ -12,8 +12,8 @@ package de.hsma.imb.ss16.tpe.del.grp7.ub1.a3;
  */
 abstract class Waehrungen {
 	public static final Waehrung DOLLAR	 = new Waehrung("US-Dollar", "$", 1.0000);
-	public static final Waehrung EURO	 = new Waehrung("Euro", "€", 1.2690);
-	public static final Waehrung YEN	 = new Waehrung("Yen", "¥", 0.0091);
+	public static final Waehrung EURO	 = new Waehrung("Euro", "â‚¬", 1.2690);
+	public static final Waehrung YEN	 = new Waehrung("Yen", "Â¥", 0.0091);
 	public static final Waehrung RUBEL	 = new Waehrung("Rubel", "RUB", 0.0255);
 	public static final Waehrung FRANKEN = new Waehrung("Schweizer Franken", "CHF", 1.0509);
 }

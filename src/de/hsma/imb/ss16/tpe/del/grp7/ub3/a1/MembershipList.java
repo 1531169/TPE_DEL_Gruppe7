7556e6cd3928379facbe65f041fdcb1e36b17edb
Cedric <Cedric@Cedric-PC.fritz.box>
2016-05-07 15:49:56 +0200

    changed implementation of toString, added private enum for columns

--- src/de/hsma/imb/ss16/tpe/del/grp7/ub3/a1/MembershipList.java
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub3/a1/MembershipList.java
@@ -1,12 +1,36 @@
 package de.hsma.imb.ss16.tpe.del.grp7.ub3.a1;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToIntBiFunction;
+import java.util.function.ToIntFunction;
 
 public class MembershipList extends HashMap<Integer, Member> implements Map<Integer, Member>, Iterable<Member> {
+	private enum Columname {
+		ID("ID"), 
+		FIRSTNAME("Vorname"),
+		SURNAME("Nachname"),
+		MEMBERSHIP("Mitgliedjahre");
+		
+		private String name;
+		private Columname(String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public int getNameLength() {
+			return name.length();
+		}
+	}
+	
 	private static final int TAB_LENGTH = 4;
-	private static final int MARGIN_SUM = 4;
+	private static final int DEFAULT_LENGTH = 1;
 	private static final String MARGIN_LEFT = "  ";
 	private static final String MARGIN_RIGHT = "  ";
 	private static final String BREAK_SIGN = "\n";
@@ -14,10 +38,7 @@ public class MembershipList extends HashMap<Integer, Member> implements Map<Inte
 	private static final String POST_SIGN = "s";
 	private static final String ROW_DELIMITER_SIGN = "-";
 	
-	private static final String COL_NAME_ID = "ID";
-	private static final String COL_NAME_FIRSTNAME = "Vorname";
-	private static final String COL_NAME_SURNAME = "Nachname";
-	private static final String COL_NAME_MEMBERSHIP = "Mitgliedsjahre";
+	private static final String EX_STRING_INVALID_COLNAME = "The given column does not exist.";
 	
 	private int lengthID;
 	private int lengthFN;
@@ -35,17 +56,17 @@ public class MembershipList extends HashMap<Integer, Member> implements Map<Inte
 		String delimiter = getDelimiter(lengthSum); 
 		
 		// add table head row
-		String result = delimiter + BREAK_SIGN;
-		result += getRow(COL_NAME_ID, COL_NAME_FIRSTNAME, COL_NAME_SURNAME, 
-				COL_NAME_MEMBERSHIP);
-		result += delimiter + BREAK_SIGN;
+		String result = delimiter;
+		result += getRow(Columname.ID.getName(), Columname.FIRSTNAME.getName(), 
+				Columname.SURNAME.getName(), Columname.MEMBERSHIP.getName());
+		result += delimiter;
 		
 		// bring every member in the right format
 		for(Member m : values()) {
 			result += getRow(((Integer)m.getMemberID()).toString(), m.getFirstname(), 
 					m.getSurname(), ((Integer)m.getMemberYears()).toString());
 		}
-		result += delimiter + BREAK_SIGN;
+		result += delimiter;
 		return result;
 	}
 
@@ -55,19 +76,22 @@ public class MembershipList extends HashMap<Integer, Member> implements Map<Inte
 	}
 	
 	private void refreshLengths() {
-		lengthID = getMaxIDLength();
-		lengthFN = getMaxFirstnameLength();
-		lengthSN = getMaxSurnameLength();
-		lengthMS = getMaxMembershipLength();
-		lengthSum = lengthID + lengthFN + lengthSN + lengthMS + MARGIN_SUM;
+		// refresh local vars
+		lengthID = getLengthFrom(Columname.ID);
+		lengthFN = getLengthFrom(Columname.FIRSTNAME);
+		lengthSN = getLengthFrom(Columname.SURNAME);
+		lengthMS = getLengthFrom(Columname.MEMBERSHIP);
+		// sum all lengths
+		lengthSum = lengthID + lengthFN + lengthSN + lengthMS;
+		lengthSum += MARGIN_LEFT.length() + MARGIN_RIGHT.length();
 	}
 	
-	private String getRow(String v1, String v2, String v3, String v4) {
+	private String getRow(String cell1, String cell2, String cell3, String cell4) {
 		String result = MARGIN_LEFT;
-		result += String.format(PRE_SIGN + lengthID + POST_SIGN, v1);
-		result += String.format(PRE_SIGN + lengthFN + POST_SIGN, v2);
-		result += String.format(PRE_SIGN + lengthSN + POST_SIGN, v3);
-		result += String.format(PRE_SIGN + lengthMS + POST_SIGN, v4);
+		result += String.format(PRE_SIGN + lengthID + POST_SIGN, cell1);
+		result += String.format(PRE_SIGN + lengthFN + POST_SIGN, cell2);
+		result += String.format(PRE_SIGN + lengthSN + POST_SIGN, cell3);
+		result += String.format(PRE_SIGN + lengthMS + POST_SIGN, cell4);
 		result += MARGIN_RIGHT + BREAK_SIGN;
 		return result;
 	}
@@ -77,48 +101,55 @@ public class MembershipList extends HashMap<Integer, Member> implements Map<Inte
 		for (int i = 0; i < length; i++) {
 			delimiter += ROW_DELIMITER_SIGN;
 		}
-		return delimiter;
+		return delimiter + BREAK_SIGN;
 	}
 	
-	private int getMaxIDLength() {
-		// get max length of longest ID
-		int maxID = values().stream()
-				.mapToInt(Member::getMemberID).max().orElse(1);
-		int lengthID = new Integer(maxID).toString().length();
-		if(lengthID < COL_NAME_ID.length()) {
-			lengthID = COL_NAME_ID.length();
+	private int getLengthFrom(Columname colname) {
+		int length = 1;
+		switch (colname) {
+		case ID:
+			// get max length of longest ID
+			length = new Integer(getIDLength()).toString().length();
+			break;
+		case FIRSTNAME:
+			// get max length of the longest firstname
+			length = getFSLength();
+			break;
+		case SURNAME:
+			// get max length of the longest surname
+			length = getSNLength();
+			break;
+		case MEMBERSHIP:
+			// get max length of the longest membership years
+			length = new Integer(getMSLength()).toString().length();
+			break;
+		default:
+			throw new InvalidParameterException(EX_STRING_INVALID_COLNAME);
 		}
-		return lengthID + TAB_LENGTH;
+		
+		if(length < colname.getNameLength()) {
+			length = colname.getNameLength();
+		}
+		return length + TAB_LENGTH;
 	}
 	
-	private int getMaxFirstnameLength() {
-		// get max length of the longest givenname
-		int lengthGN = values().stream().map(Member::getFirstname)
-				.mapToInt(String::length).max().orElse(1);
-		if(lengthGN < COL_NAME_FIRSTNAME.length()) {
-			lengthGN = COL_NAME_FIRSTNAME.length();
-		}
-		return lengthGN + TAB_LENGTH;
+	private int getIDLength() {
+		return values().stream()
+				.mapToInt(Member::getMemberID).max().orElse(DEFAULT_LENGTH);
 	}
 	
-	private int getMaxSurnameLength() {
-		// get max length of the longest surname
-		int lengthSN = values().stream().map(Member::getSurname)
-				.mapToInt(String::length).max().orElse(1);
-		if(lengthSN < COL_NAME_SURNAME.length()) {
-			lengthSN = COL_NAME_SURNAME.length();
-		}
-		return lengthSN + TAB_LENGTH;
+	private int getFSLength() {
+		return values().stream().map(Member::getFirstname)
+				.mapToInt(String::length).max().orElse(DEFAULT_LENGTH);
 	}
 	
-	private int getMaxMembershipLength() {
-		// get max length of the longest membership years
-		int maxMembership = values().stream()
-				.mapToInt(Member::getMemberYears).max().orElse(1);
-		int lengthMS = new Integer(maxMembership).toString().length();
-		if(lengthMS < COL_NAME_MEMBERSHIP.length()) {
-			lengthMS = COL_NAME_MEMBERSHIP.length();
-		}
-		return lengthMS;
+	private int getSNLength() {
+		return values().stream().map(Member::getSurname)
+				.mapToInt(String::length).max().orElse(DEFAULT_LENGTH);
+	}
+	
+	private int getMSLength() {
+		return values().stream()
+				.mapToInt(Member::getMemberYears).max().orElse(DEFAULT_LENGTH);
 	}
 }

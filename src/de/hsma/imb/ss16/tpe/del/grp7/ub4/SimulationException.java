f745b85c1f3f30ffc15f820d66d3841d1b0956ce
Loic Donkeng <Loic Donkeng@141.19.176.39>
2016-05-31 17:48:16 +0200

    change of visibility from default to public because access needed by SimulationTest from package de.hsma.imb ...simulationTest.

--- src/de/hsma/imb/ss16/tpe/del/grp7/ub4/SimulationException.java
+++ src/de/hsma/imb/ss16/tpe/del/grp7/ub4/SimulationException.java
@@ -1,7 +1,7 @@
 package de.hsma.imb.ss16.tpe.del.grp7.ub4;
 
 @SuppressWarnings("serial")
-class SimulationException extends Exception {
+public class SimulationException extends Exception {
 
 	public SimulationException() {
 	}
